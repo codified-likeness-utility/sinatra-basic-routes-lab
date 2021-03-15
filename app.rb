76566e1fee96b280059e8e18b873f56eeb1a3b2a
Ian Davies <mr.daviesian@gmail.com>
2021-03-15 14:29:23 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is Ian."
+    end
+
+    get '/hometown' do
+        "My hometown is Cincinnati."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Oh, Chrsstmas Tree'."
+    end
+
+
 end
